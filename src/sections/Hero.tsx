0af441eb4519dd13b128f2dd5584517b15b22cff
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-03-02 23:45:28 -0300

    Add files via upload

---
+++ src/sections/Hero.tsx
@@ -0,0 +1,13 @@
+export default function HeroSection() {
+  return (
+    <section className="text-center">
+      <h1 className="text-5xl text-cyberpink font-bold">
+        Art-Synt Cyberpunk Store
+      </h1>
+
+      <p className="mt-4 text-lg font-roboto">
+        Futuristic virtual commerce powered by Firebase.
+      </p>
+    </section>
+  )
+}
