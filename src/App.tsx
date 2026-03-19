56c2ff98c7a5b6e4981349e22a03e2dc9bcc9720
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-03-19 20:47:51 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,19 @@
-import { Routes, Route, useLocation } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Access from './pages/Access'
 import Products from './pages/Products'
 import Contact from './pages/Contact'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import MainLayout from './layout/Main'
 
 export default function App() {
-  const location = useLocation()
-  const isHome = location.pathname === '/'
-
   return (
-    <div className="min-h-screen bg-black text-white">
-      <Header />
-
-      <main className="w-full">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/acceso" element={<Access />} />
-          <Route path="/productos" element={<Products />} />
-          <Route path="/contacto" element={<Contact />} />
-        </Routes>
-      </main>
-
-      {isHome ? <Footer /> : null}
-    </div>
+    <MainLayout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/acceso" element={<Access />} />
+        <Route path="/productos" element={<Products />} />
+        <Route path="/contacto" element={<Contact />} />
+      </Routes>
+    </MainLayout>
   )
 }
