b6eaa106b54df49cb76427e87c7293b956d4588a
Lu <117615336+lu-developer476@users.noreply.github.com>
2026-03-19 21:27:49 -0300

    Update Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -13,7 +13,7 @@ export default function HeroSection() {
         <div className="space-y-4">
           <h1 className="max-w-5xl text-4xl font-semibold uppercase leading-tight tracking-wide text-purple-100 md:text-6xl">
             La carne y el metal, entrelazados
-            Aumentadno tus propias capacidades
+            Fortalece tus propias capacidades
           </h1>
 
           <p className="max-w-3xl text-lg italic text-purple-200/90">
